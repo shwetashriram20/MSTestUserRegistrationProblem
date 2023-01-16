42f8bf20e1808ee72ccdc4e7ec37d55868d59fe0
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-16 16:44:47 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,3 @@
-﻿using StackPrime;
-
-namespace Stack
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            PrimeNumber numbers = new PrimeNumber();
-            numbers.CheckPrime();
 
 
 
-        }
-    }
-}
