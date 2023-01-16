9ca0c27cf5df0e0e7ddfd17f4989cd913e7aee54
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-16 17:59:39 +0530

    Add files via upload

---
+++ RegexSample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class RegexSample
+    {
+        string pattern = "^[A-Za-z]{2,}$";
+        public void Validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Validating the first name");
+            ValidatingFirstName();
+            Console.WriteLine("Validating The Last Name");
+            ValidatingLastName();
+
+        }
+
+
+
+        public void ValidatingFirstName()
+        {
+            Regex Regex = new Regex(pattern);
+            Console.WriteLine("Enter the first name:");
+            string input = Console.ReadLine();
+            bool res = Regex.IsMatch(input);
+            if (res)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void ValidatingLastName()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Enter the last name : ");
+            string input1 = Console.ReadLine();
+            bool res1 = regex.IsMatch(input1);
+            if (res1)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
