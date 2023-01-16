8105be153eacf1a537c219b0898128b2c554d22f
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-16 16:17:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using StackPrime;
+
+namespace Stack
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrimeNumber numbers = new PrimeNumber();
+            numbers.CheckPrime();
+
+
+
+        }
+    }
+}
