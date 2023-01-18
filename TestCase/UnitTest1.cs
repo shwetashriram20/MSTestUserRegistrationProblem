59929a8419a3495c9b374ac42c3903610a645027
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-18 11:53:11 +0530

    Add files via upload

--- TestCase/UnitTest1.cs
+++ TestCase/UnitTest1.cs
@@ -1,1 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegisteration;
 
+namespace UserRegisteration
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //validation for first Name
+        [TestMethod]
+        [DataRow("Shweta", "Shweta")]
+        [DataRow("Shw", "Shw")]
+        [DataRow("s", null)]
+        [DataRow("Shwt0e", null)]
+
+        public void ValidateFirstname(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingFirstName(a);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
