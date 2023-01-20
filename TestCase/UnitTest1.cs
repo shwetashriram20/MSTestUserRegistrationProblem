48c6cfe5358ab501de8f4fd108c3df510491ef5c
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-20 21:37:37 +0530

    Add files via upload

--- TestCase/UnitTest1.cs
+++ TestCase/UnitTest1.cs
@@ -1,34 +1,48 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UserRegisteration;
 
-namespace TestProject2
+namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         //validation for first Name
         [TestMethod]
-        [DataRow("Shweta", "Shweta")]
-        [DataRow("Shw", "Shw")]
+        [DataRow("Shalini", "Shalini")]
+        [DataRow("Sha", "Sha")]
         [DataRow("s", null)]
-        [DataRow("Shwt09", null)]
+        [DataRow("Shalu05", null)]
 
         public void ValidateFirstname(string a, string expected)
         {
+            try
+            {
 
-            var actual = RegexSample.ValidatingFirstName(a);
-            Assert.AreEqual(expected, actual);
+                var actual = RegexSample.ValidatingFirstName(a);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (CustomException actual)
+            {
+                Assert.AreEqual(expected, actual.message);
+            }
         }
         //Validating for Last Name
         [TestMethod]
-        [DataRow("taeh", "taeh")]
-        [DataRow("ta", null)]
-        [DataRow("tae", "tae")]
-        [DataRow("tae09", null)]
+        [DataRow("Venkatesh", "Venkatesh")]
+        [DataRow("Ve", null)]
+        [DataRow("Venkat", "Venkat")]
+        [DataRow("Venkat05", null)]
         public void ValidateUserLastname(string a, string expected)
         {
-            var actual = RegexSample.ValidatingLastName(a);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                var actual = RegexSample.ValidatingLastName(a);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (CustomException actual)
+            {
+                Assert.AreEqual(expected, actual.message);
+            }
         }
         //Validation for Email
         [TestMethod]
@@ -57,8 +71,15 @@ namespace TestProject2
 
         public void ValidateUserEmail(string a, string expected)
         {
-            var actual = RegexSample.ValidatingEmailId(a);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                var actual = RegexSample.ValidatingEmailId(a);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (CustomException actual)
+            {
+                Assert.AreEqual(expected, actual.message);
+            }
         }
 
         //Validation for Phone Number
@@ -72,35 +93,133 @@ namespace TestProject2
         [DataRow("91 984290505000000", null)]
         public void ValidateUserPhoneNumber(string a, string expected)
         {
-            var actual = RegexSample.ValidatingPhoneNum(a);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                var actual = RegexSample.ValidatingPhoneNum(a);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (CustomException actual)
+            {
+                Assert.AreEqual(expected, actual.message);
+            }
         }
 
         ////Validation for Password
         [TestMethod]
-        [DataRow("Shwe@123", "Shwe@123")]
-        [DataRow("Shwe#ta123", "Shwe#ta123")]
-        [DataRow("Shw@etaa1S ", "Shw@etaa1S")]
-        [DataRow("@Shwetae129", "@Shwetae129")]
-        [DataRow("Shw-eta123", "Shw-eta123")]
-        [DataRow("shweta123)@1234", null)]
-        [DataRow("Shw@eta", null)]
-        [DataRow("shw123", null)]
-        [DataRow("shw@123", null)]
-        [DataRow("@shw#12shw", null)]
-        [DataRow(")shwe12", null)]
-        [DataRow(")shwEt12", null)]
-        [DataRow("shw-et@12S", null)]
-        [DataRow("shwET@s", null)]
+        [DataRow("Shalu@123", "Shalu@123")]
+        [DataRow("Shai#lini123", "Sha#lini123")]
+        [DataRow("Sha@lini1S ", "Sha@lini1S")]
+        [DataRow("@Shalini129", "@Shalini129")]
+        [DataRow("Sha-lini123", "Sha-lini123")]
+        [DataRow("shalini123)@1234", null)]
+        [DataRow("Sha@lini", null)]
+        [DataRow("sha123", null)]
+        [DataRow("sha@123", null)]
+        [DataRow("@sha#12sha", null)]
+        [DataRow(")shalu12", null)]
+        [DataRow(")shaLu12", null)]
+        [DataRow("sha-lu@12S", null)]
+        [DataRow("shaLU@s", null)]
 
         public void ValidateUserPassword(string a, string expected)
         {
-            var actual = RegexSample.ValidatingPassWord(a);
-            Assert.AreEqual(expected, actual);
+            try
+            {
+                var actual = RegexSample.ValidatingPassWord(a);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (CustomException actual)
+            {
+                Assert.AreEqual(expected, actual.message);
+            }
+        }
+        [TestMethod]
+        public void Test_Method_Object_Creation_RegexSample()
+        {
+            object expected = new RegexSample();
+            UserRegistrationFactory factory = new UserRegistrationFactory();
+            object actual = factory.CreateObjectForRegexSample("UserRegistration.RegexSample", "RegexSample");
+            expected.Equals(actual);
+
+        }
+        //Test for parameterconstructor invoked using object created
+        [TestMethod]
+        public void Test_Method_Parameteized_Constructor()
+        {
+            object expected = new RegexSample("RegularExpression");
+            UserRegistrationFactory factory = new UserRegistrationFactory();
+            object actual = factory.CreateParameterizedConstructor("UserRegistration.RegexSample", "RegexSample", "RegularExpression");
+            actual.Equals(expected);
+        }
+
+        [TestMethod]
+        public void Reflection_Return_Default_Constructor_No_Constructor_Found()
+        {
+            string expected = "No constructor found";
+            object obj = null;
+            try
+            {
+                UserRegistrationFactory factory = new UserRegistrationFactory();
+                obj = factory.CreateObjectForRegexSample("UserRegistration.RegexSample", "RegexSam");
+
+            }
+            catch (CustomException actual)
+            {
+                Assert.AreEqual(expected, actual.Message);
+            }
+        }
+        [TestMethod]
+        public void Reflection_Return_Default_Constructor_No_Class_Found()
+        {
+            string expected = "No class found";
+            object obj = null;
+            try
+            {
+                UserRegistrationFactory factory = new UserRegistrationFactory();
+                obj = factory.CreateObjectForRegexSample("UserRegistration.RegexSae", "RegexSample");
+
+            }
+            catch (CustomException actual)
+            {
+                Assert.AreEqual(expected, actual.Message);
+            }
+        }
+        [TestMethod]
+        public void Reflection_Return_Parameterized_Class_Invalid()
+        {
+            string message = "Regular Expression";
+            string expected = "No class found";
+            object actual = null;
+            try
+            {
+                UserRegistrationFactory factory = new UserRegistrationFactory();
+                actual = factory.CreateParameterizedConstructor("UserRegistration.RegexSae", "RegexSample", message);
+
+            }
+            catch (CustomException actual1)
+            {
+                Assert.AreEqual(expected, actual1.Message);
+            }
+        }
+        [TestMethod]
+        public void Reflection_Return_Parameterized_Constructor_Invalid()
+        {
+            string message = "Regular Expression";
+            string expected = "No constructor found";
+            object actual = null;
+            try
+            {
+                UserRegistrationFactory factory = new UserRegistrationFactory();
+                actual = factory.CreateParameterizedConstructor("UserRegistration.RegexSample", "RegexSam", message);
+
+            }
+            catch (CustomException actual1)
+            {
+                Assert.AreEqual(expected, actual1.Message);
+            }
         }
 
 
 
     }
 }
-
